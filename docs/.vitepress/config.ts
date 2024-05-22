79b4cb2602d2ee1f578d414549c21163b82ccb3f
Zzhiren <zzhirensos@163.com>
2024-05-22 17:22:35 +0800

    feat: assetsDir

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   title: 'Vue Core',
   appearance: 'dark',
   srcDir: './src',
-  assetsDir: '/vue-core/static',
+  assetsDir: '../static',
 
   markdown: {
     theme: {
